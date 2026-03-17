81560c91c549f39b55fd169c826ce111c483ef67
Nathaneal <nathaneali@outlook.com>
2026-03-17 15:23:42 -0400

    post comp

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,16 +1,137 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import static edu.wpi.first.units.Units.*;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
 
 public class ShooterSubsystem extends SubsystemBase {
-    public static final double DEFAULT_SHOOT_RPM = 3000;
 
-    public Command shootBothCommand(double rpm) { return Commands.none(); }
-    public Command spinUpAndWaitCommand(double rpm) { return Commands.none(); }
-    public Command shootLeftCommand(double rpm) { return Commands.none(); }
-    public Command shootRightCommand(double rpm) { return Commands.none(); }
-    public Command stopCommand() { return Commands.none(); }
-    public boolean atSpeed(double rpm) { return false; }
+    public static final double DEFAULT_SHOOT_RPM = Constants.ShooterConstants.kDefaultShootRPM;
+    private static final AngularVelocity RPM_TOLERANCE = RPM.of(Constants.ShooterConstants.kRPMTolerance);
+
+    // --- LEFT SHOOTER ---
+    private final SparkMax leftMotor = new SparkMax(
+        Constants.ShooterConstants.kLeftMotorId, MotorType.kBrushless);
+
+    private final SmartMotorControllerConfig leftSMCConfig = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withClosedLoopController(
+            Constants.ShooterConstants.kP,
+            Constants.ShooterConstants.kI,
+            Constants.ShooterConstants.kD)
+        .withTelemetry("ShooterLeftMotor", TelemetryVerbosity.HIGH)
+        .withGearing(new MechanismGearing(GearBox.fromReductionStages(1)))
+        .withMotorInverted(false)
+        .withIdleMode(MotorMode.COAST)
+        .withStatorCurrentLimit(Amps.of(Constants.ShooterConstants.kStatorCurrentLimit));
+
+    private final SmartMotorController leftSMC = new SparkWrapper(
+        leftMotor, DCMotor.getNEO(1), leftSMCConfig);
+
+    private final FlyWheel leftLauncher = new FlyWheel(
+        new FlyWheelConfig(leftSMC)
+            .withDiameter(Inches.of(4))
+            .withTelemetry("LeftLauncher", TelemetryVerbosity.HIGH));
+
+    // --- RIGHT SHOOTER ---
+    private final SparkMax rightMotor = new SparkMax(
+        Constants.ShooterConstants.kRightMotorId, MotorType.kBrushless);
+
+    private final SmartMotorControllerConfig rightSMCConfig = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withClosedLoopController(
+            Constants.ShooterConstants.kP,
+            Constants.ShooterConstants.kI,
+            Constants.ShooterConstants.kD)
+        .withTelemetry("ShooterRightMotor", TelemetryVerbosity.HIGH)
+        .withGearing(new MechanismGearing(GearBox.fromReductionStages(1)))
+        .withMotorInverted(true)
+        .withIdleMode(MotorMode.COAST)
+        .withStatorCurrentLimit(Amps.of(Constants.ShooterConstants.kStatorCurrentLimit));
+
+    private final SmartMotorController rightSMC = new SparkWrapper(
+        rightMotor, DCMotor.getNEO(1), rightSMCConfig);
+
+    private final FlyWheel rightLauncher = new FlyWheel(
+        new FlyWheelConfig(rightSMC)
+            .withDiameter(Inches.of(4))
+            .withTelemetry("RightLauncher", TelemetryVerbosity.HIGH));
+
+    public ShooterSubsystem() {}
+
+    /** Returns true when both shooters are at target speed */
+    public boolean atSpeed(double targetRPM) {
+        return leftLauncher.isNear(RPM.of(targetRPM), RPM_TOLERANCE).getAsBoolean() &&
+               rightLauncher.isNear(RPM.of(targetRPM), RPM_TOLERANCE).getAsBoolean();
+    }
+
+    /** Spin both launchers at the given RPM, stop when command ends */
+    public Command shootBothCommand(double targetRPM) {
+        return run(() -> {
+            leftSMC.setVelocity(RPM.of(targetRPM));
+            rightSMC.setVelocity(RPM.of(targetRPM));
+        }).finallyDo(() -> {
+            leftSMC.setDutyCycle(0);
+            rightSMC.setDutyCycle(0);
+        }).withName("Shooter.ShootBoth");
+    }
+
+    /** Spin up and wait until both are at speed */
+    public Command spinUpAndWaitCommand(double targetRPM) {
+        return Commands.parallel(
+            leftLauncher.runTo(RPM.of(targetRPM), RPM_TOLERANCE),
+            rightLauncher.runTo(RPM.of(targetRPM), RPM_TOLERANCE)
+        ).withName("Shooter.SpinUpAndWait");
+    }
+
+    /** Fire just the left launcher */
+    public Command shootLeftCommand(double targetRPM) {
+        return run(() -> leftSMC.setVelocity(RPM.of(targetRPM)))
+            .finallyDo(() -> leftSMC.setDutyCycle(0))
+            .withName("Shooter.ShootLeft");
+    }
+
+    /** Fire just the right launcher */
+    public Command shootRightCommand(double targetRPM) {
+        return run(() -> rightSMC.setVelocity(RPM.of(targetRPM)))
+            .finallyDo(() -> rightSMC.setDutyCycle(0))
+            .withName("Shooter.ShootRight");
+    }
+
+    /** Stop both shooters */
+    public Command stopCommand() {
+        return runOnce(() -> {
+            leftSMC.setDutyCycle(0);
+            rightSMC.setDutyCycle(0);
+        }).withName("Shooter.Stop");
+    }
+
+    @Override
+    public void periodic() {
+        leftLauncher.updateTelemetry();
+        rightLauncher.updateTelemetry();
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        leftLauncher.simIterate();
+        rightLauncher.simIterate();
+    }
 }
